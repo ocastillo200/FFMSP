1a27b7f6dcd31c2ef214f861a59d7a653a89513a
Oscar Castillo <132367173+ocastillo200@users.noreply.github.com>
2024-11-21 12:36:50 -0300

    fixed bugs in logic

--- src/libs/grasp.cpp
+++ src/libs/grasp.cpp
@@ -36,19 +36,26 @@ std::pair<int, std::string> GRASP(const std::vector<std::string> &inputStrings, 
     int stringLength = inputStrings[0].size();
     std::string bestSolution;
     int bestFitness = -1;
-
+    
     auto totalStart = std::chrono::high_resolution_clock::now(); // Tiempo total desde el inicio
 
     for (int iter = 0; iter < maxIterations; ++iter)
     {
         auto greedyResult = constructGreedySolution(stringLength, alphabet, inputStrings, epsilon, t);
-        std::string currentSolution = greedyResult.second;
-        int currentFitness = greedyResult.first;
-        std::string localsearchSolution;
+        if (greedyResult.first > bestFitness){
+            std::string currentSolution = greedyResult.second;
+            int currentFitness = greedyResult.first;
+            bestFitness = currentFitness;
+            bestSolution = currentSolution;
+            auto solutionTime = std::chrono::high_resolution_clock::now();
+            double elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(solutionTime - totalStart).count();
+            std::cout
+                        << "Fitness: " << bestFitness << std::endl
+                        << "Tiempo en que se encontró la solución: " << elapsedTime << "s" << std::endl;
+        }
         int localsearchFitness;
-
+        std::string localsearchSolution;
         std::tie(localsearchSolution, localsearchFitness) = localSearch(currentSolution, currentFitness, inputStrings, alphabet, t);
-
         if (localsearchFitness > bestFitness)
         {
             bestSolution = localsearchSolution;
