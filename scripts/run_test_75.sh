3c846b23303e441e2e0c84b3e5ae7cb1e286d4d7
Francisco Cea <fcoceak@gmail.com>
2024-12-03 18:33:29 -0300

    finish 0.75

--- scripts/run_test_75.sh
+++ scripts/run_test_75.sh
@@ -9,7 +9,7 @@ test_path="../build/tests"
 instances_path="../instancias/"
 
 for case in "${cases[@]}"; do
-    for mode in "genetic"; do
+    for mode in "hybrid"; do
         mkdir -p results
         output_file="results/${mode}-${case}-0.75.txt"
         rm -f $output_file    # 
@@ -17,7 +17,7 @@ for case in "${cases[@]}"; do
             if [ "$th" != "${th_list[0]}" ]; then
                 echo "\n" >> "$output_file"
             fi
-            $test_path -th $th -n $case -mode $mode -f $instances_path -t 10 -e 0.99 -p 1000 -m 0.01 -c 0.14 -g 100 -tn 1 >> "$output_file"
+            $test_path -th $th -n $case -mode $mode -f $instances_path -t 10 -e 0.99 -p 200 -m 0.01 -c 0.22 -g 2000 -tn 1 >> "$output_file"
         done
     done
 done
